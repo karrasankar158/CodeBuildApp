d35fca8f70a7d702e0e564ba90b7736d7ca216ad
karrasankar09@gmail.com <karrasankar09@gmail.com>
2021-11-30 18:16:05 +0530

    buildspecfile

--- src/main/java/com/controller/TestApp.java
+++ src/main/java/com/controller/TestApp.java
@@ -1,14 +1,12 @@
 package com.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
 public class TestApp {
 	
-	@GetMapping("/show")
+	@GetMapping("/")
 	public String displayMessage() {
 		
 		return "Welcome to AWS";
